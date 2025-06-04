2f09c39903980142505d28faad89e292fa3879fa
Omkar Vaigankar <42296321+omkarvaigankar8@users.noreply.github.com>
2025-06-05 00:19:02 +0530

    tailwind shadcn and other packages installed

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');`
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
